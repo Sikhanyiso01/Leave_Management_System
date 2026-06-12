585707de7e7f96f8c48a1d54af34e7ebf7739b0c
Saziso Buthelezi <sazisoshenge1@gmail.com>
2026-06-12 17:00:40 +0200

    Add project files.

---
+++ Leave_Management_System.web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Leave_Management_System.web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+
+namespace Leave_Management_System.web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Hanyani Shabalala AKA (Miss Kitty)",
+                DateOfBirth = new DateTime(2004, 06, 03)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ Leave_Management_System.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leave_Management_System.web.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
